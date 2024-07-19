2c5192954824d94199bae43454743b8383ea9096
widyantorooficial <widy45rr@gmail.com>
2024-07-19 07:41:07 +0700

    bot

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
-use Telegram\Bot\FileUpload\InputFile;
-use Carbon\Carbon;
 
 class TelegramController extends Controller
 {
