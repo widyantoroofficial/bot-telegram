1c74060e93b2d2ff8750459cc3b26880275239d2
widyantorooficial <widy45rr@gmail.com>
2024-07-19 08:04:23 +0700

    bot

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Telegram;
 use App\Http\Controllers\Controller;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Telegram\Bot\FileUpload\InputFile;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class TelegramController extends Controller
 {
--- app/Http/Controllers/Telegram/WebhookController.php
+++ app/Http/Controllers/Telegram/WebhookController.php
@@ -8,6 +8,12 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class WebhookController extends Controller
 {
+    public function setwebhook()
+    {
+        $response = Telegram::setWebhook(['url' => 'https://bot.latihanserver.my.id/webhook']);
+
+        return $response ? 'Webhook Berhasil Diatur' : 'Failed to set webhook';
+    }
     public function example()
     {
         $response = Telegram::setWebhook(['url' => 'https://bot.latihanserver.my.id/example']);
